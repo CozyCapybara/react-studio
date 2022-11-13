ea49f64742a3bb5d0545b289c47fd22f305c1eef
thekojomojo <kojo_dadzie@brown.edu>
2022-11-13 17:06:28 -0500

    finished studio

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,12 @@
 // TODO: create a component that displays a single bakery item
+export function BakeryItem({item, index, updateCart, price, setPrice}) {
+  return(
+    <div>
+      <h3>{item.name}</h3>
+      <img src={item.image}/>
+      <p>{item.description}</p>
+      <p>{item.price}</p>
+      <button onClick={() => {updateCart(index); setPrice(price+item.price)}}>Add to Cart</button>
+    </div>
+  )
+}
